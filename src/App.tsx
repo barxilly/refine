1a4c27ed316371fb08f947de6dac95bcfc04509f
Ben Smith <ben@barxsmith.sbs>
2025-09-01 14:13:09 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { Flex, Image, MantineProvider, Stack } from "@mantine/core";
+import "./App.css";
+import "@mantine/core/styles.css";
+
+function App() {
+  return (
+    <MantineProvider forceColorScheme="dark">
+      <Stack w="95vw" h="100vh">
+        <Flex className="topbar" justify="space-between">
+          <div>Siena</div>
+          <div style={{ position: "relative" }}>
+            0% Complete
+            <Image
+              className="lulog"
+              src="https://lumonmerchandising.com/cdn/shop/files/lumon_logo-black-png_4a7c637e-96dd-45e7-a02e-eba67561a7e7.png?v=1743065319"
+              h="3.6em"
+              w="auto"
+              pos="absolute"
+              top="-1em"
+              right="-6.5em"
+            />
+          </div>
+        </Flex>
+      </Stack>
+    </MantineProvider>
+  );
+}
+
+export default App;
